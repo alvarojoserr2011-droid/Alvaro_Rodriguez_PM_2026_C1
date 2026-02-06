55e2ca0c02a00cbafd133eccb7061fa54aa55559
alvarojoserr2011-droid <alvarojose.rr2011@gmail.com>
2026-02-06 02:58:30 -0400

    Add files via upload

---
+++ Tarea3.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+main()
+{
+int I;
+float SAL, NOM;
+NOM = 0;
+for (I=1; I<=15; I++)
+    printf("\nSalario del profesor%d:\t", I);
+    scanf("%f", &SAL,);
+    NOM = NOM + SAL;
+  printf("\nEl total de la nomina es: %.2f", NOM);
+}
+
+#include <stdio.h>
+
+main()
+{
+int N, i;
+int SUM = 0;
+printf("Ingrese la cantidad de numeros:\t");
+     scanf("%d", &N);
+     for (i = 1; i <= N; i++)
+        if (NUM > 0)
+     suma += num;
+     }
+     printf("\nLa suma de los numeros positivos es: %d", SUM);
+     {
+     }
+
+
+#include <stdio.h>
+
+main()
+{
+int N, i;
+float PAG;
+float SUM = 0;
+printf("Ingrese primer pago:\t");
+scanf("%d", &N);
+while (PAG)
+    for (i = 1; i <= N; i++)
+        printf("Ingrese el pago %d: ", i);
+    scanf("%f", &PAG);
+printf("\nSuma total de los pagos del mes: %.2f", SPA);
+}
+
+
+#include <stdio.h>
+
+main()
+{
+int NUM;
+long CUA, SUC = 0;
+printf("\nIngrese un numero entero -0 para terminar-:\t");
+scanf("%d", &NUM);
+while (NUM)
+    CUA = pow (NUM, 2);
+    printf("%d al cubo es %ld", NUM, CUA);
+    printf("\nIngrese un numero entero -0 para terminar-:\t");
+    scanf("%d", &NUM);
+
+    printf("\nLa suma de los cuadrados es: %ld", SUC);
+}
+
+
+#include <stdio.h>
+
+{
+    int main()
+int I = 0;
+float NOM = 0;
+float PROM;
+int SAL = 0;
+printf("Ingrese el salario del profesor -0 para terminar-:\t");
+scanf("%f", &SAL);
+do
+    NOM = NOM + SAL;
+    I = I + 1;
+    printf("Salario del profesor -0 para terminar- :\t");
+    scanf("%f", &SAL);
+
+    while (SAL);
+    PRO = NOM / I;
+    printf("\nNomina total: %.2f \t", NOM);
+    printf("\nPromedio de salarios: %.2f", PRO);
+}
+
+
+#include <stdio.h>
+
+int main()
+{
+int I, N;
+float LAN, SLA = 0;
+do
+    printf("Ingrese el numero de lanzamientos:\t");
+    scanf("%d", &N);
+{
+while (N < 1 || N > 11);
+for (i = 1; i <= N; i++)
+    printf("\nIngrese el lanzamiento %d: ", I);
+    scanf("%f", &LAN);
+    SLA = SLA + LAN;
+
+ SLA = SLA / N;
+printf("\nEl promedio de lanzamientos es: %.2f", SLA);
+}
+}
